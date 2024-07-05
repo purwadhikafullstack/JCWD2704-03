74cfa6221eb76b3948f2605bcfe32cfd37ee28b2
astnab <astnab@gmail.com>
2024-07-05 16:15:05 +0700

    feat: web signup

--- apps/web/src/app/auth/login/user/page.tsx
+++ apps/web/src/app/auth/login/user/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function page() {
-  return <div>ini login user</div>;
+  return (
+    <div>
+      ini login user
+      <div className="font-bold">test</div>
+      <button className="bg-red-300 font-bold">submit</button>
+    </div>
+  );
 }
 
 export default page;
