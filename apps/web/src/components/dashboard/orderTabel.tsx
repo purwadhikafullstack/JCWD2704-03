e131283e013b5db75b2fe5ea6a7aaa51a21187c1
ccandradewi <cornelia.candradewi@outlook.com>
2024-07-18 02:40:05 +0700

    feat: status order

--- apps/web/src/components/dashboard/orderTabel.tsx
+++ apps/web/src/components/dashboard/orderTabel.tsx
@@ -3,9 +3,11 @@ import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import { Order } from '@/models/reservation.model';
 import { axiosInstance } from '@/libs/axios.config';
+import { useRouter } from 'next/navigation';
 
 function OrderTable() {
   const [orders, setOrders] = useState<Order[]>([]);
+  const router = useRouter();
   const fetchOrderData = async () => {
     try {
       const response = await axiosInstance().get(
@@ -21,24 +23,38 @@ function OrderTable() {
   useEffect(() => {
     fetchOrderData();
   }, []);
-
+  const handleCancelled = (orderId: string) => {
+    router.push(`/dashboard/order/cancel/${orderId}`);
+  };
+  const handleDenied = (orderId: string) => {
+    router.push(`/dashboard/order/denied/${orderId}`);
+  };
   const formatDate = (dateString: string | null) => {
     if (!dateString) return '-';
     return dayjs(dateString).format('DD/MM/YY');
   };
-  const renderStatus = (status: string) => {
+  const renderStatus = (status: string, orderId: string) => {
     switch (status) {
       case 'awaiting_confirmation':
         return (
           <button
-            className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-normal rounded-xl text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+            className="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 md:w-60 font-normal rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-yellow-900"
             type="button"
+            onClick={() => handleDenied(orderId)}
           >
             AWAITING CONFIRMATION
           </button>
         );
       case 'pending_payment':
-        return <span className="text-blue-500">PENDING PAYMENT</span>;
+        return (
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 md:w-60 font-normal rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:focus:ring-blue-800"
+            type="button"
+            onClick={() => handleCancelled(orderId)}
+          >
+            PENDING PAYMENT
+          </button>
+        );
       case 'success':
         return <span className="text-green-500">SUCCESS</span>;
       case 'cancelled':
@@ -47,73 +63,76 @@ function OrderTable() {
         return status.replace('_', ' ');
     }
   };
+  const headers = [
+    'Invoice',
+    'Name',
+    'Property',
+    'Room Type',
+    'Room',
+    'Reservation Date',
+    'CheckIn',
+    'CheckOut',
+    'Status',
+  ];
 
   return (
-    <div className="overflow-x-auto md:overflow-x-auto-hidden">
-      <div className=" text-3xl font-semibold pb-3">All Transaction</div>
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-sm text-center text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Invoice
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Property
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Room Type
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Room
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Reservation Date
-            </th>
-            <th scope="col" className="px-6 py-3">
-              CheckIn
-            </th>
-            <th scope="col" className="px-6 py-3">
-              CheckOut
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order) => (
-            <tr
-              key={order.id}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            >
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {order.invoice_id.toLocaleUpperCase()}
-              </td>
-              <td className="px-6 py-4 text-center">{order.user.first_name}</td>
-              <td className="px-6 py-4 text-center">{order.property.name}</td>
-              <td className="px-6 py-4 text-center">
-                {order.room.type.toUpperCase()}
-              </td>
-              <td className="px-6 py-4 text-center">{order.total_room}</td>
-              <td className="px-6 py-4 text-center">
-                {formatDate(order.payment_date)}
-              </td>
-              <td className="px-6 py-4 text-center">
-                {formatDate(order.checkIn_date)}
-              </td>
-              <td className="px-6 py-4 text-center">
-                {formatDate(order.checkOut_date)}
-              </td>
-              <td className="p-4 text-sm uppercase">
-                {renderStatus(order.status)}
-              </td>
+    <div className="flex flex-col gap-6">
+      <div className=" text-3xl font-semibold pb-3 text-[#263C94] mt-10">
+        All Transaction
+      </div>
+      <div className="overflow-x-auto">
+        <table className="w-full text-sm border text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-sm text-center text-gray-700  bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              {headers.map((header) => (
+                <th
+                  key={header}
+                  scope="col"
+                  className="px-6 py-3 uppercase text-md"
+                >
+                  {header}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr
+                key={order.id}
+                className={`${
+                  index % 2 === 0
+                    ? 'bg-white dark:bg-gray-800'
+                    : 'bg-blue-50 dark:bg-gray-700'
+                } border-b dark:border-gray-700`}
+              >
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {order.invoice_id.toLocaleUpperCase()}
+                </td>
+                <td className="px-6 py-4 text-center">
+                  {order.user.first_name}
+                </td>
+                <td className="px-6 py-4 text-center">{order.property.name}</td>
+                <td className="px-6 py-4 text-center">
+                  {order.room.type.toUpperCase()}
+                </td>
+                <td className="px-6 py-4 text-center">{order.total_room}</td>
+                <td className="px-6 py-4 text-center">
+                  {formatDate(order.payment_date)}
+                </td>
+                <td className="px-6 py-4 text-center">
+                  {formatDate(order.checkIn_date)}
+                </td>
+                <td className="px-6 py-4 text-center">
+                  {formatDate(order.checkOut_date)}
+                </td>
+                <td className="p-4 text-sm uppercase">
+                  {renderStatus(order.status, order.id)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
