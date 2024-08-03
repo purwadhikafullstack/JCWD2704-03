b0e3c38f669f25ebf78f66922ed0c01f4e89ab99
astnab <astnab@gmail.com>
2024-08-03 20:41:28 +0700

    feat: added loading

--- apps/web/src/components/auth/verification/ResendEmail.tsx
+++ apps/web/src/components/auth/verification/ResendEmail.tsx
@@ -43,6 +43,13 @@ export default function ResendEmail() {
   };
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden py-6 sm:py-12 bg-white">
+      {isLoading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      )}
       <div className="max-w-xl px-3 text-center flex flex-col items-center">
         <a className="py-4">
           <img
